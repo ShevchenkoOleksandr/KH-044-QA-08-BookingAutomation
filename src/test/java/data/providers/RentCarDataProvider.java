e805e17cd209a529cd84b9547e5eaad38c5005a0
igorm <gormakova.ira@gmail.com>
2022-07-10 11:11:42 +0200

    final changes (test data)

--- src/test/java/data/providers/RentCarDataProvider.java
+++ src/test/java/data/providers/RentCarDataProvider.java
@@ -6,7 +6,7 @@ public class RentCarDataProvider {
     @DataProvider(name = "orderData")
     public static Object[][] orderData() {
         return new Object[][]{
-                {"Kraków",
+                {"Krakow",
                         "Kraków, Lesser Poland, Poland",
                         "2022-10-10",
                         "2022-12-02"
