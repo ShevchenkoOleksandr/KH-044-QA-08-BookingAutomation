c9bd99743038fccb8b639c75dcec7cb3c98b1b25
igorm <gormakova.ira@gmail.com>
2022-07-11 14:47:23 +0200

    resultpage (asserts)

--- src/test/java/pages/ResultsPage.java
+++ src/test/java/pages/ResultsPage.java
@@ -14,62 +14,43 @@ import static org.hamcrest.Matchers.equalToIgnoringWhiteSpace;
 import static org.hamcrest.Matchers.greaterThan;
 
 public class ResultsPage extends BasePage {
-    private WebElement pickUpLocation;
-    private WebElement pickUpDate;
-    private WebElement dropOffLocation;
-    private WebElement dropOffDate;
-    private WebElement countCars;
+
+    protected By locationUp = By.cssSelector("div[data-testid='pick-up-location']");
+    protected By locationOff = By.cssSelector("div[data-testid='drop-off-location']");
+    protected By dateUpResult = By.cssSelector("div[data-testid='pick-up-date']");
+    protected By dateOffResult = By.cssSelector("div[data-testid='drop-off-date']");
+    protected By resultString = By.cssSelector("div[data-testid='page-title']");
 
 
     public ResultsPage(WebDriver driver) {
         super(driver);
-        initElements();
     }
 
-    public void initElements() {
-        pickUpLocation = driver.findElement(By.cssSelector("div[data-testid='pick-up-location']"));
-        pickUpDate = driver.findElement(By.cssSelector("div[data-testid='pick-up-date']"));
-        dropOffLocation = driver.findElement(By.cssSelector("div[data-testid='drop-off-location']"));
-        dropOffDate = driver.findElement(By.cssSelector("div[data-testid='drop-off-date']"));
-        countCars = driver.findElement(By.cssSelector("div[data-testid='page-title']"));
-    }
-
-
-    public String getPickUpLocationText() {
-        return pickUpLocation.getText();
-    }
-
-    public String getPickUpDateText() {
-        return pickUpDate.getText();
-    }
-
-    public String getDropOffLocationText() {
-        return dropOffLocation.getText();
-    }
-
-    public String getDropOffDateText() {
-        return dropOffDate.getText();
-    }
-
-    public String getCountCarsText() {
-        return countCars.getText();
-    }
 
     public ResultsPage checkLocations(String locationUpOff, String locationResults) {
-        assertThat(getPickUpLocationText(), equalToIgnoringWhiteSpace(locationUpOff));
-        assertThat(getDropOffLocationText(), equalToIgnoringWhiteSpace(locationUpOff));
-        assertThat(StringToResults.stringToLocation(getCountCarsText()), equalToIgnoringWhiteSpace(locationResults));
+        waitPageRefresh();
+        WebElement pickUpLocation = driver.findElement(locationUp);
+        WebElement dropOffLocation = driver.findElement(locationOff);
+        WebElement countCars = driver.findElement(resultString);
+        assertThat(pickUpLocation.getText(), equalToIgnoringWhiteSpace(locationUpOff));
+        assertThat(dropOffLocation.getText(), equalToIgnoringWhiteSpace(locationUpOff));
+        assertThat(StringToResults.stringToLocation(countCars.getText()), equalToIgnoringWhiteSpace(locationResults));
         return this;
     }
 
     public ResultsPage checkDates(String dateUp, String dateOff) {
-        assertThat(StringToResults.stringToDate(getPickUpDateText()), equalToIgnoringWhiteSpace(dateUp));
-        assertThat(StringToResults.stringToDate(getDropOffDateText()), equalToIgnoringWhiteSpace(dateOff));
+        waitPageRefresh();
+        WebElement pickUpDate = driver.findElement(dateUpResult);
+        WebElement dropOffDate = driver.findElement(dateOffResult);
+        assertThat(StringToResults.stringToDate(pickUpDate.getText()), equalToIgnoringWhiteSpace(dateUp));
+        assertThat(StringToResults.stringToDate(dropOffDate.getText()), equalToIgnoringWhiteSpace(dateOff));
         return this;
     }
 
     public ResultsPage checkCountCars() {
-        assertThat(StringToResults.stringToResults(getCountCarsText()), greaterThan(0));
+        waitPageRefresh();
+        WebElement countCars = driver.findElement(resultString);
+        assertThat(StringToResults.stringToResults(countCars.getText()), greaterThan(0));
         return this;
     }
 
